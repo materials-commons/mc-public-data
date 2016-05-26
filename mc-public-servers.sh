bfb62ec84c355eeb40e53545f06f870f73790c1f
tammasr <tammasr@umich.edu>
2016-05-26 16:13:25 -0400

    update server file

--- mc-public-servers.sh
+++ mc-public-servers.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 
 
-export DB_PORT =  30815
-export DB_CONNECTION = "localhost:$DB_PORT"
-export DB_FILE = /db_dumps/rethinkdb_dump_2016-04-20T13:30:11.tar.gz
+export DB_PORT=30815
+export DB_CONNECTION="localhost:$DB_PORT"
+export DB_FILE=/db_dumps/rethinkdb_dump_2016-04-20T13:30:11.tar.gz
 export DB_HTTP_PORT=8090
 export DB_CLUSTER_PORT=31815
 
-export API_PORT = 5000
-export NODE_PATH = /src/backend
+export API_PORT=5000
+export NODE_PATH=/src/backend
 
-function startServers()
+
+function startServers
 {
    echo "Starting API Server"
    cd $NODE_PATH;
+   echo pwd
    node --harmony app.js
 
    echo "Starting Rethinkdb"
@@ -22,3 +24,6 @@ function startServers()
    echo "Gulp build"
    gulp serve:dist
 }
+
+startServers
+
