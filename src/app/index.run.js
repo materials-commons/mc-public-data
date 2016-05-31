e2651dd00950b0ae30cbeb01d72cae6ac6150287
V. Glenn Tarcea <gtarcea@umich.edu>
2016-05-31 13:57:25 -0400

    Run code formatter.

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -1,29 +1,29 @@
 export function runBlock($log, userService, $state, $rootScope, Restangular) {
-  'ngInject';
-  $log.debug('runBlock end');
+    'ngInject';
+    $log.debug('runBlock end');
 
-  if (userService.isAuthenticated()) {
-    Restangular.setDefaultRequestParams(['post', 'get', 'put', 'remove'], {apikey: userService.apikey()});
-  }else{
-    Restangular.setDefaultRequestParams(['get', 'post'], {apikey: userService.apikey()});
-  }
-
-  $rootScope.$on('$stateChangeStart', function (event, toState) {
-    $rootScope.email = userService.email();
-    $rootScope.image = userService.image();
-    // search bar under the top navigation will show up only for certain routes
-    checkNavigationSearchBar(toState)
-  });
-
-  $rootScope.$on('$stateChangeError', function () {
-    $state.go("main.login");
-  });
-
-  function checkNavigationSearchBar(toState) {
-    if (toState.name.match(/(home)/g) || toState.name.match(/(search)/g)) {
-      $rootScope.showSearchBar = false;
+    if (userService.isAuthenticated()) {
+        Restangular.setDefaultRequestParams(['post', 'get', 'put', 'remove'], {apikey: userService.apikey()});
     } else {
-      $rootScope.showSearchBar = true;
+        Restangular.setDefaultRequestParams(['get', 'post'], {apikey: userService.apikey()});
     }
-  }
+
+    $rootScope.$on('$stateChangeStart', function(event, toState) {
+        $rootScope.email = userService.email();
+        $rootScope.image = userService.image();
+        // search bar under the top navigation will show up only for certain routes
+        checkNavigationSearchBar(toState)
+    });
+
+    $rootScope.$on('$stateChangeError', function() {
+        $state.go("main.login");
+    });
+
+    function checkNavigationSearchBar(toState) {
+        if (toState.name.match(/(home)/g) || toState.name.match(/(search)/g)) {
+            $rootScope.showSearchBar = false;
+        } else {
+            $rootScope.showSearchBar = true;
+        }
+    }
 }
