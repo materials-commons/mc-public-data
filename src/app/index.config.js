227459ceffd02bf6b7e90d438dd79056bcfa8186
V. Glenn Tarcea <gtarcea@umich.edu>
2016-06-06 12:48:53 -0400

    Change toastr timeout value.

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -6,7 +6,7 @@ export function config ($logProvider, toastrConfig, RestangularProvider, $window
 
   // Set options third-party lib
   toastrConfig.allowHtml = true;
-  toastrConfig.timeOut = 3000;
+  toastrConfig.timeOut = 750;
   toastrConfig.positionClass = 'toast-top-right';
   toastrConfig.preventDuplicates = true;
   toastrConfig.progressBar = true;
