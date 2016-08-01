efe19f2d72af76d5fbd769bcaa6e2e988f773344
V. Glenn Tarcea <glenn.tarcea@gmail.com>
2016-07-31 20:02:00 -0400

    Add agreement text.

--- src/app/components/sign/validate/validate.controller.js
+++ src/app/components/sign/validate/validate.controller.js
@@ -1,6 +1,6 @@
 export class ValidateController {
     /*@ngInject*/
-    constructor($stateParams, accountsService, userService, $state, Restangular, toastr) {
+    constructor($stateParams, accountsService, userService, $state, Restangular, toastr, $uibModal) {
         this.$stateParams = $stateParams;
         this.validationId = $stateParams.validation_id;
         this.accountsService = accountsService;
@@ -8,6 +8,7 @@ export class ValidateController {
         this.$state = $state;
         this.Restangular = Restangular;
         this.toastr = toastr;
+        this.$uibModal = $uibModal;
         this.password1 = '';
         this.password2 = '';
         this.accountsService.getUserRegistrationAccount(this.validationId)
@@ -22,24 +23,37 @@ export class ValidateController {
             return;
         }
 
-        this.accountsService.setUserFromRegistrationData(this.validationId, this.password1)
-            .then(
-                () => {
-                    this.userService.login(this.registration.email, this.password1)
-                        .then(
-                            (user)=> {
-                                this.userService.setAuthenticated(true, user.plain());
-                                this.Restangular.setDefaultRequestParams(['post', 'get', 'put', 'remove'], {apikey: this.userService.apikey()});
-                                this.$state.go('home.top');
-                            },
-                            (err) => {
-                                this.toastr.options = {"closeButton": true};
-                                this.toastr.error(err.data, this.user.email);
-                            }
-                        );
-                },
-                (e) => this.message = e.data.error
-            );
+        var modalInstance = this.$uibModal.open({
+            animation: true,
+            templateUrl: 'app/components/sign/validate/agreement.html',
+            controller: AgreementController,
+            controllerAs: 'ctrl',
+            size: 'lg'
+        });
+
+        modalInstance.result.then(
+            ()=> {
+                this.accountsService.setUserFromRegistrationData(this.validationId, this.password1)
+                    .then(
+                        () => {
+                            this.userService.login(this.registration.email, this.password1)
+                                .then(
+                                    (user)=> {
+                                        this.userService.setAuthenticated(true, user.plain());
+                                        this.Restangular.setDefaultRequestParams(['post', 'get', 'put', 'remove'], {apikey: this.userService.apikey()});
+                                        this.$state.go('home.top');
+                                    },
+                                    (err) => {
+                                        this.toastr.options = {"closeButton": true};
+                                        this.toastr.error(err.data, this.user.email);
+                                    }
+                                );
+                        },
+                        (e) => this.message = e.data.error
+                    );
+            },
+            () => this.$state.go('home.top')
+        );
     }
 
 
@@ -52,11 +66,26 @@ export class ValidateController {
             this.message = "Password entries do not match";
             return false;
         }
-        if (this.password1.length < 6) {
-            this.message = "Password must be at least 6 character in length";
-            return false;
-        }
+        //if (this.password1.length < 6) {
+        //    this.message = "Password must be at least 6 character in length";
+        //    return false;
+        //}
         this.message = null;
         return true;
+    }
+}
+
+class AgreementController {
+    /*@ngInject*/
+    constructor($uibModalInstance) {
+        this.$uibModalInstance = $uibModalInstance;
+    }
+
+    agree() {
+        this.$uibModalInstance.close();
+    }
+
+    cancel() {
+        this.$uibModalInstance.dismiss('cancel');
     }
 }
