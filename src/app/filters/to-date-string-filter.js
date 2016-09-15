dbd98086f99f621b251c82021112da22115d044c
Terry Weymouth <weymouth@umich.edu>
2016-09-15 08:23:45 -0400

    Fixed formatting of date, e.g. for date of publication

--- src/app/filters/to-date-string-filter.js
+++ src/app/filters/to-date-string-filter.js
@@ -2,7 +2,21 @@ export function toDateStringFilter() {
   return function(input) {
     if (input) {
       var t = input;
-      return new Date(t * 1000).toDateString();
+      var type = typeof t;
+      if (type === 'string') {
+        return new Date(t).toDateString();
+      }
+      if (type === 'Date') {
+        return t.toDateString();
+      }
+      var date = null;
+      try {
+        date = new Date(t * 1000);
+      } catch (ignore) {}
+      if (date) {
+        return date.toDateString();
+      }
+      return "";
     }
     return "";
   };
