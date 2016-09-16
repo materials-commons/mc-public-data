b83047c3899180d912be3a40d56c94fee4b4f3c6
V. Glenn Tarcea <glenn.tarcea@gmail.com>
2016-09-16 14:16:21 -0400

    Make output look nicer including filtering out empty items and displaying appropriate information if there is no setup information.

--- src/app/details/outline/mcpub-process-details-setup.component.js
+++ src/app/details/outline/mcpub-process-details-setup.component.js
@@ -1,12 +1,28 @@
 export class MCPubProcessDetailsSetupComponentController {
     /*@ngInject*/
-    constructor() {
+    constructor($filter) {
+        this.$filter = $filter;
+        this.processSetupWithValues = [];
+        this.processHasSetupValues = false;
+    }
 
+    $onInit() {
+        for (let i = 0; i < this.processSetup.length; i++) {
+            this.processSetupWithValues.push(this.processSetup.slice(i)[0]);
+            let properties = this.processSetupWithValues[i].properties;
+            this.processSetupWithValues[i].properties = this.$filter('filter')(properties, this.removeEmpty());
+        }
+
+        for (let i = 0; i < this.processSetupWithValues.length; i++) {
+            if (this.processSetupWithValues[i].properties.length) {
+                this.processHasSetupValues = true;
+                break;
+            }
+        }
     }
 
     removeEmpty() {
         return function(prop) {
-            console.log('prop');
             return prop.value !== '';
         }
     }
