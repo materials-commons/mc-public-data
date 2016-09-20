3b93f52accc99823ecf5b92a3c28389c91f47ec7
Terry Weymouth <weymouth@umich.edu>
2016-09-20 11:38:26 -0400

    Small changes: use publisher (by name, if possible); update display for download to say 'download dataset'; papers as a list

--- src/app/directives/property-value-directive.js
+++ src/app/directives/property-value-directive.js
@@ -1,49 +1,46 @@
 export function PropertyValueDirective() {
-  'ngInject';
+    'ngInject';
 
-  let directive = {
-    restrict: 'E',
-    templateUrl: 'app/directives/property-value.html',
-    scope: {
-      property: "@",
-      value: "="
-    },
-    controller: PropertyValueController,
-    controllerAs: 'ctrl',
-    bindToController: true
-  };
-  return directive;
+    let directive = {
+        restrict: 'E',
+        templateUrl: 'app/directives/property-value.html',
+        scope: {
+            property: "@",
+            value: "="
+        },
+        controller: PropertyValueController,
+        controllerAs: 'ctrl',
+        bindToController: true
+    };
+    return directive;
 }
 
 class PropertyValueController {
-  constructor(userService) {
-    'ngInject';
+    constructor(userService) {
+        'ngInject';
 
-    this.userService = userService;
-    this.user = this.userService.isAuthenticated();
-  }
+        this.userService = userService;
+        this.user = this.userService.isAuthenticated();
+    }
 
-  formatAuthor(a) {
-    return a.lastname + ", " + a.firstname + " (" + a.affiliation + ")";
-  }
+    formatAuthor(a) {
+        return a.lastname + ", " + a.firstname + " (" + a.affiliation + ")";
+    }
 
-  formatAuthorList(authors) {
-    return authors.map(this.formatAuthor).join('; ');
-  }
+    formatAuthorList(authors) {
+        if (authors.length == 0) return "(no authors are listed)";
+        return "By " + authors.map(this.formatAuthor).join('; ');
+    }
 
-  formatDate(date) {
-    return
-  }
+    formatAuthors(authors) {
+        return authors.map(a => `${a.lastname}, ${a.firstname} (${a.affiliation})`).join('; ');
+    }
 
-  formatAuthors(authors) {
-    return authors.map(a => `${a.lastname}, ${a.firstname} (${a.affiliation})`).join('; ');
-  }
+    getPublisherName(userId) {
+        return userId;
+    }
 
-  getPublisherName(userId) {
-    return userId;
-  }
-
-  urlForDownload(datasetId) {
-    return "api/pub/datasets/download/" + datasetId + "?apikey=" + this.userService.apikey();
-  }
+    urlForDownload(datasetId) {
+        return "api/pub/datasets/download/" + datasetId + "?apikey=" + this.userService.apikey();
+    }
 }
